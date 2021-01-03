fc99a05221d9ed917fc43b67b92274b7006df617
Neda Motamediraad <motamediraad@gmail.com>
2021-01-03 12:02:53 +0330

    modify title position to center

--- phonebook/src/backend/addContact.py
+++ phonebook/src/backend/addContact.py
@@ -71,7 +71,7 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def getData(self):
-        print("Added New Contact.\n {0:20s}|{1:30s}  |{2:11s}  |{3:30s}".format('Name', 'Family', 'Phone', 'Email'))
+        print("Added New Contact.\n {0:^20}|{1:^30}  |{2:^11}  |{3:^30}".format('Name', 'Family', 'Phone', 'Email'))
         print("-------------------------------------------------------------------------------------------")
         print("{0:20s} | {1:30s} | {2:11d} | {3:30s}".format(self.fname.text(), self.lname.text(), int(self.phone.text()), self.adress.text()))
         #print('Added New Contact: ', self.fname.text(), self.lname.text(), self.phone.text(), self.adress.text())
