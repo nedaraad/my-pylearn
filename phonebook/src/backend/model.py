68b49b381cac8b9f64a8cbaadbc95b3b0f15f79b
Neda Motamediraad <motamediraad@gmail.com>
2021-01-04 13:42:57 +0330

    create class person to create person object

---
+++ phonebook/src/backend/model.py
@@ -0,0 +1,7 @@
+
+class Person():
+    def __init__(self, fname, lname, phone, adress ):
+        self.fname = fname
+        self.lname = lname
+        self.phone = phone
+        self.adress = adress
